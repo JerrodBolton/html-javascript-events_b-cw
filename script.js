510d9fea5bcfc4cd5947bf226289caa951325b2c
JerrodBolton <rodrod1@jerrodbltonsmbp.attlocal.net>
2021-08-31 20:57:23 -0500

    Exercise 1

---
+++ script.js
@@ -0,0 +1,32 @@
+// alert("I hope everyone is okay")
+
+// ### Exercise 1:
+// ==================================================================================================================================================================================================================
+
+// //  Global labels wh
+// let label = document.querySelector("label");
+// let textArea = document.querySelector("textarea"); 
+// // I need to get the id and make them equal 
+// textArea.addEventListener("keypress", (evt) => {
+//     // label.textContent = 
+//     // console.log(evt.key);// this is a check
+//     let words = evt;
+//     label.textContent = words;
+    
+    
+// }); //I need this here because is get the key when someone press on the key.
+ 
+
+
+// // label.appendChild(textArea);
+
+// ==================================================================================================================================================================================================================
+ 
+// ### Exercise 2:
+
+let label = document.querySelector("label");
+let textArea = document.querySelector("textarea"); 
+
+// I needed to get the 
+
+
